d095dbc728c4ea6fe390ba92817c6ac94d1a0aea
j8298c <juliocmojica@gmail.com>
2016-10-22 20:01:52 -0400

    still needs function to check answer

--- app.js
+++ app.js
@@ -1,87 +1,70 @@
+//make better use of data objects for questions and answers
+//fix questions
 var Model = {
- questions: [
-  ["What was marios first game"],
-  ["Name of the villian who wants the Triforce"],
-  ["Who replaced Mike Tyson in Punch-Out!"],
-  ["What is the contra secret 30 life code"],
-  ["Family name of Castlevania cast"]
-],
- answers: [["DonkeyKong", "Super Mario Brothers", "Mario Brothers", 'Duckhunt'],["Luigi", "Wario", "Gannon", "Bowser"], ["Mr. Dream", "SodaPopinski", "BaldBull", "Yoshi"], ["up up down dow right right left left", "up up down down left right left right", "left left right up down right right start", "up up left left right right down down"], ["Van helsing", "Crusader","Belmont", "Dirk"]
-],
-answerKey : {
-  1: "DONKEYKONG",
-  2: 'GANON',
-  3: "MR. DREAM",
-  4: "UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT",
-  5: "BELMONT"
+questions : [
+  {
+    text: "In which game did Super Mario make his first apperance?",
+    choices: ["Mario Brothers", "Super Mario Brothers", "Donkey Kong", "Super Mario Land"],
+    correctChoice: 'C'
+  }, {
+    text: "In the land of Hyrule what is the name of the villian who wants the Triforce?",
+    choices: ["Medusa", "Gannon", "Link", "Bowser"],
+    correctChoice: "B"
+  }, {
+    text: "In Metal Gear the agent Snake was sent to stop what weapon?",
+    choices: ["The Black Pearl", "2nd Heaven", "Snake", "Metal gear"],
+    correctChoice: "D"
+  }, {
+    text: "Nintendo replaced Mike Tyson in Punch-out with this fighter?",
+    choices: ["Mr. Dream", "Soda Popinski", "Bald Bull", "Piston Honda"],
+    correctChoice: "A"
+  }, {
+    text: "Little Mac and his trainer took on every fighter in Punch-out what was the name of his trainer?",
+    choices: ["Trainer", "Doc", "Mario", "Yoshi"],
+    correctChoice: "B"
+  }, {
+    text: "During the early days of castlevania this family was cursed, and needed to defeat Dracula to lift the curse?",
+    choices: ["Simon", "Van Helsing", "Belmont", "El Pope"],
+    correctChoice: "C"
+  },
+]
 }
-};
 
 //global variables
 var i = 0;
+var j = 0;
 var score = 0;
 
-//runs the whole game || should make controller
 function runGame(){
-  $(".start").on('click', function(){
-    $(".startScreen").css("display", 'none');
-    $('.question').append('<h2>' + Model.questions[i] + '</h2>');
-    questions();
-    displayChoices();
-    updateScore();
-  })
+  startGame();
+  nextQuestion();
+  displayTest();
 }
 
-//sets questions from model on view
-function questions(){
+function startGame(){
+  $('.start').on('click', function(){
+    $('.startScreen').css('display', 'none');
+  });
+}
+
+function displayTest(){
+  $('.question').text(Model.questions[i].text);
+  $('.choices').each(function(j){
+    $(this).text(Model.questions[i].choices[j]);
+    j++
+  })
+};
+
+function nextQuestion(){
   $('.nextquestion').on('click', function(){
-      if(i <= 3) {
-        i++
-        displayQuestions();
-        displayChoices();
-        updateScore();
-      } else {
-        endGame();
-      }
+    i++
+    if(i <= Model.questions.length -1){
+      displayTest();
+    } else {
+      alert("The game has ended your score is " + score);
+      i = 0;
+    }
   })
 }
 
-//adds new question to div and ands last question
-function displayQuestions(){
-  $('.question').empty();
-  $('.question').append('<h2>' + Model.questions[i] + '</h2>');
-  $('.choices').empty();
-}
-
-
-//iteratres over answers array and displays them in li
-function displayChoices(){
-  for(var j = 0; j < Model.answers.length -1; j++) {
-  $(".choices").append('<li>' + Model.answers[i][j] + '</li><br>');
-  }
-}
-//function to endGame
-function endGame(){
-  if(i == Model.questions.length -1){
-    alert("game over your score is " + score + ". Refresh your screen to play again");
-    i = 0;
-  } else {
-    questions();
-  }
-}
-
-//function to updateScore
-function updateScore(){
-  var answer = $('.answerbox').val().toUpperCase();
-  for(var k = 0; k < Model.answerKey.length -1; k++){
-    $('.nextquestion').on('click', function(){
-      if(answerKey[k] == answer){
-        score+=1;
-        console.log(score);
-      } else {
-        score = 0;
-      }
-    })
-  }
-}
 runGame();

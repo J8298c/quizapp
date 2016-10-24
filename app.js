35ed746984f482c5c9d62d9341f4a75c82714ae9
j8298c <juliocmojica@gmail.com>
2016-10-23 23:53:24 -0400

    fixed check answer function

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ questions : [
   {
     text: "In which game did Super Mario make his first apperance?",
     choices: ["Mario Brothers", "Super Mario Brothers", "Donkey Kong", "Super Mario Land"],
-    correctChoice: 'C'
+    correctChoice: 'c'
   }, {
     text: "In the land of Hyrule what is the name of the villian who wants the Triforce?",
     choices: ["Medusa", "Gannon", "Link", "Bowser"],
-    correctChoice: "B"
+    correctChoice: "b"
   }, {
     text: "In Metal Gear the agent Snake was sent to stop what weapon?",
     choices: ["The Black Pearl", "2nd Heaven", "Snake", "Metal gear"],
-    correctChoice: "D"
+    correctChoice: "d"
   }, {
     text: "Nintendo replaced Mike Tyson in Punch-out with this fighter?",
     choices: ["Mr. Dream", "Soda Popinski", "Bald Bull", "Piston Honda"],
-    correctChoice: "A"
+    correctChoice: "a"
   }, {
     text: "Little Mac and his trainer took on every fighter in Punch-out what was the name of his trainer?",
     choices: ["Trainer", "Doc", "Mario", "Yoshi"],
-    correctChoice: "B"
+    correctChoice: "b"
   }, {
     text: "During the early days of castlevania this family was cursed, and needed to defeat Dracula to lift the curse?",
     choices: ["Simon", "Van Helsing", "Belmont", "El Pope"],
-    correctChoice: "C"
+    correctChoice: "c"
   },
 ]
 }
@@ -34,6 +34,7 @@ questions : [
 var i = 0;
 var j = 0;
 var score = 0;
+var qCount = 0
 
 function runGame(){
   startGame();
@@ -65,17 +66,31 @@ function nextQuestion(){
     } else {
       alert("The game has ended your score is " + score);
       i = 0;
+      resetGame(startGame());
     }
   });
 
   function checkAnswer(){
-      if($('input').prop('id')[i] === Model.questions[i].correctChoice) {
-        console.log('correctChoice');
-      } else {
-        alert('wrong')
-      }
-  };
-
+   console.log("j:", j);
+   console.log("i:", i);
+   var checkSelection = $('input[name=choice]:checked').val();
+     if(checkSelection == Model.questions[qCount].correctChoice) {
+     // if($('input').prop('id')[j] === Model.questions[j].correctChoice) {
+       score +=1;
+       qCount += 1;
+     } else {
+       alert('wrong')
+       qCount +=1;
+     }
+ };
+  function resetGame(){
+    $('.test').css('display', 'none');
+    $('body').append('<button class="reset">Reset</button> ');
+    $('.reset').on('click', function(){
+      $('.startScreen').css('display', 'block');
+      startGame();
+    })
+  }
 }
 
 runGame();

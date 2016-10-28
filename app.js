6dcc11f3a0804bcb1517fddee7343cfffd61621d
j8298c <juliocmojica@gmail.com>
2016-10-27 20:34:48 -0400

    add game images to question and h1 to dom at end with score

--- app.js
+++ app.js
@@ -5,46 +5,56 @@ questions : [
   {
     text: "In which game did Super Mario make his first apperance?",
     choices: ["Mario Brothers", "Super Mario Brothers", "Donkey Kong", "Super Mario Land"],
-    correctChoice: 'c'
+    correctChoice: 'c',
+    gameImage: "images/mario.png"
   }, {
     text: "In the land of Hyrule what is the name of the villian who wants the Triforce?",
     choices: ["Medusa", "Gannon", "Link", "Bowser"],
-    correctChoice: "b"
+    correctChoice: "b",
+    gameImage: "images/zelda.jpg"
   }, {
     text: "In Metal Gear the agent Snake was sent to stop what weapon?",
     choices: ["The Black Pearl", "2nd Heaven", "Snake", "Metal gear"],
-    correctChoice: "d"
+    correctChoice: "d",
+    gameImage: "images/metalgear.jpg"
   }, {
     text: "Nintendo replaced Mike Tyson in Punch-out with this fighter?",
     choices: ["Mr. Dream", "Soda Popinski", "Bald Bull", "Piston Honda"],
-    correctChoice: "a"
+    correctChoice: "a",
+    gameImage: 'images/punchout.png'
   }, {
     text: "Little Mac and his trainer took on every fighter in Punch-out what was the name of his trainer?",
     choices: ["Trainer", "Doc", "Mario", "Yoshi"],
-    correctChoice: "b"
+    correctChoice: "b",
+    gameImage: 'images/littlemac.png'
   }, {
     text: "During the early days of castlevania this family was cursed, and needed to defeat Dracula to lift the curse?",
     choices: ["Simon", "Van Helsing", "Belmont", "El Pope"],
-    correctChoice: "c"
+    correctChoice: "c",
+    gameImage: 'images/castlevania.png'
   }, {
     text: "What was the code to gain 30 lives in Contra?",
     choices: ["Up Up Down Down Left Up Down", 'Up Up Down Down Left Right Left Right', "Down Down Up Left Right", "Up Up Left Right Left Right"],
-    correctChoice: "b"
+    correctChoice: "b",
+    gameImage: 'images/contra.jpg'
   }, {
     text: "Super Mario Brothers 3 was accompanied by a special accessory what was that accessory?",
     choices: ["Nintendo Cube", "Mario's Raccon Tail", "Fred Savage", "Power Glove"],
-    correctChoice: 'd'
+    correctChoice: 'd',
+    gameImage: 'images/mario3.png'
   }, {
     text: "In the game Bad Dudes the two brothers had a saying do you know what it was?",
     choices: ["I want to rock and roll", 'Cowabunga', 'Whos Bad', 'We got this'],
-    correctChoice: 'c'
+    correctChoice: 'c',
+    gameImage: 'images/baddudes.jpg'
   }, {
     text: "In kung-fu what sound effect would occur if the player died?",
     choices: ["Signing everyone was kung-fu fighting", 'Sinster laugh', 'Sound of martial artist', 'Micheal Jacksons beat it'],
-    correctChoice: 'b'
+    correctChoice: 'b',
+    gameImage: 'images/kungfu.png'
   }
 ]
-}
+};
 
 //global variables
 var i = 0;
@@ -66,6 +76,7 @@ function startGame(){
 
 function displayTest(){
   $('.question').text(Model.questions[i].text);
+  $('.gamepics').attr('src', Model.questions[i].gameImage);
   $('.choices').each(function(j){
     $(this).text(Model.questions[i].choices[j]);
     j++
@@ -80,32 +91,25 @@ function nextQuestion(){
       checkAnswer();
         $('input').prop("checked", false);
     } else {
-      alert("The game has ended your score is " + score);
-      i = 0;
-      resetGame();
+      gameOver();
     }
   });
+}
 
+function gameOver(){
+  $('.test').css('display', 'none');
+  $('.gamepics').remove();
+  $('.game-over ').css("display", "block");
+  $('.player-score').text("Your Score is " + score + "!!!!");
+}
   function checkAnswer(){
-   console.log("j:", j);
-   console.log("i:", i);
    var checkSelection = $('input[name=choice]:checked').val();
      if(checkSelection == Model.questions[qCount].correctChoice) {
-     // if($('input').prop('id')[j] === Model.questions[j].correctChoice) {
        score +=1;
        qCount += 1;
      } else {
        qCount +=1;
      }
- };
-  function resetGame(){
-    $('.test').css('display', 'none');
-    $('body').append('<button class="reset">Reset</button> ');
-    $('.reset').on('click', function(){
-      startGame();
-      runGame();
-    })
-  }
-}
+ }
 
 runGame();
